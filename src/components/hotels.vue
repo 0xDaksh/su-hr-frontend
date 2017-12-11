04c376eb0322f4275b0add1aecd9242a0265dbaf
Daksh Miglani <hello@dak.sh>
2017-12-11 22:10:33 +0530

    work on hotels a bit.

--- src/components/hotels.vue
+++ src/components/hotels.vue
@@ -5,35 +5,7 @@
       <h2>{{error.whattosay}}</h2>
     </div>
     <div v-show="!loading && !error.is">
-      <v-flex xs12 sm6 offset-sm3>
-        <v-card>
-          <v-card-media
-            class="white--text"
-            height="200px"
-            src="/static/doc-images/cards/docks.jpg"
-          >
-            <v-container fill-height fluid>
-              <v-layout fill-height>
-                <v-flex xs12 align-end flexbox>
-                  <span class="headline">Top 10 Australian beaches</span>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-media>
-          <v-card-title>
-            <div>
-              <span class="grey--text">Number 10</span><br>
-              <span>Whitehaven Beach</span><br>
-              <span>Whitsunday Island, Whitsunday Islands</span>
-            </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn flat color="orange">Share</v-btn>
-            <v-btn flat color="orange">Explore</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-
+      // display hotels here!
     </div>
   </div>
 </template>
@@ -84,11 +56,10 @@ a
         })
       }
     },
-    mounted() {
+    beforeMount() {
       setTimeout(() => {
-        console.log('y?')
         this.getHotels()
-      }, 500)
+      }, 1000)
     }
   }
 </script>
